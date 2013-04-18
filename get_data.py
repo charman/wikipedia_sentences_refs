338743b83e104c8b9e405cdf84064e3cf82e49f7
Luke Orland <orluke@gmail.com>
2013-04-18 11:00:09 -0400

    preliminary version of code that replaces refs with tags.

--- get_data.py
+++ get_data.py
@@ -6,6 +6,7 @@ import nltk
 #import wikipydia
 import re
 from wpTextExtractor import wiki2sentences
+from bs4 import BeautifulSoup
 
 title = "Barack Obama"
 
@@ -19,25 +20,80 @@ def scrape_wikitext(title):
     return revs[-1].text.encode('utf8')
 
 
+def remove_refs_with_no_url(wikitext):
+    pass
+
+def remove_refs_with_no_url(wikitext):
+    return re.sub(
+        r'<ref>[^<]*(?!url)</ref>',
+        '',
+        wikitext,
+        re.MULTILINE | re.UNICODE,
+        )
+
+def replace_refs_with_url(wikitext):
+
+    # def next_ref_num():
+    #     ref_num = 0
+    #     while True:
+    #         yield ref_num
+    #         ref_num += 1
+
+    # return
+    #     re.sub(
+    #         r'<ref>[^<]*url\s*=\s*()[^<]*</ref>',
+    #         ' coereftag ',
+    #         wikitext,
+    #         re.MULTILINE | re.UNICODE,
+    #         ),
+    #     )
+
+    # FYI: Alternative implementation
+
+    soup = BeautifulSoup(wikitext)
+    refs = soup.find_all('ref')
+    ref_urls = []
+    for i,r in enumerate(refs):
+        r.replace_with(' coeref%i ' % i)
+        ref_urls.append('http://www.wildplaces.co.uk/descent/descent168.html')
+    return soup.text, ref_urls
+
+def split_sentences(wikitext):
+    sent_detector = nltk.data.load('tokenizers/punkt/%s.pickle' % 'english').tokenize
+    all_sentences = wiki2sentences(wikitext, sent_detector, True)[0]
+    results = []
+    for sentence in all_sentences:
+        if sentence != 'References':
+            results.append(sentence)
+        else:
+            break
+    return results
+
+
 if __name__ == '__main__':
     title = 'Aquamole Pot'
 
     # Step @: download page in wikitext format
     wikitext = scrape_wikitext(title)
-    print wikitext
-    #wikitext = wikipydia.query_text_raw(title, 'en')['text']
+
+    # Step @: Replace {{ref}} tags that have a url with a word like
+    wikitext
+    # Step @: Split into sentences (using nltk)
+    sentences = split_sentences(wikitext)
+
+    # coerefN where N is an integer numeral.
+    ref_num = 0
 
     # Step @: Get the citations
-    # replace the citation/ref with a placeholder before doing sentence detection.
-    # - if follows a period, insert placeholder before period.
-    ref = re.compile(r'<ref>.*</ref>', re.MULTILINE & re.DOTALL)
+    # - step through the raw wiki text matching each of the next instance of
+    # the words from the sent_detector step, ignoring {{text}} and
+    # <ref>no url</ref>, and capturing each <ref>no url</ref> and the sentence
+    # number it belongs to.
+
+    # NO: replace the citation/ref with a placeholder before doing sentence detection.
+    # NO: - if follows a period, insert placeholder before period.
 
     # Step @: Pruned everything starting with "References"
-
-    # Step @: Split into sentences (using nltk)
-    sent_detector = nltk.data.load('tokenizers/punkt/%s.pickle' % 'english').tokenize
-    for sent in wiki2sentences(wikitext,sent_detector,True)[0]:
-        print(sent.encode('utf-8'))
 
     # ? prune sentences shorter than a threshold (to get rid of headers?
 
@@ -46,3 +102,9 @@ if __name__ == '__main__':
 
     # Step @: 
 
+
+    # If the first token of new line is coerefN, then it belongs to the
+    # previous line.
+
+# TODO:
+# use something to run the {{}} code
--- test.py
+++ test.py
@@ -1,38 +1,12 @@
 # -*- encoding: utf-8 -*-
 import get_data
+import re
 import unittest
 
-
-class TestAquamolePot(unittest.TestCase):
-
-    def setUp(self):
-        self.title = 'https://en.wikipedia.org/wiki/Aquamole_Pot'
-
-    def test_sentences(self):
-        expect = [
-            'Aquamole Pot is a cave in West Kingsdale, North Yorkshire, '
-                'England.',
-            'It was originally explored from below by cave divers who had '
-                'negotiated 550 feet (170 m) of sump passage from Rowten Pot '
-                'in 1974, to discover a high aven above the river passage.',
-            'History',
-            'The 130 feet (40 m) aven was scaled in 1980 using poles, '
-                'ladders and hand bolting kits, and a radio location '
-                'transmitter placed at the highest point.',
-            'Having discovered it was 180 feet (55 m) from, and 180 feet (55 '
-                'm) below Jingling Pot, the aven was renamed Aquamole Aven '
-                'instead of Jingling Avens.',
-            'Work restarted in 2000 when divers who were keen on a quick '
-                'route to the sump beyond, rescaled the avens to a higher '
-                'point, and radio located a position to 50 feet (15 m) below '
-                'the moor.',
-            'It was finally connected to the surface in June 2002.'
-        ]
-        actual = get_data.scrape_wikitext('Aquamole Pot')
-        self.assertEqual(expect, actual)
+from ddt import ddt, data
 
 
-text = """Work restarted in 2000 when divers who were keen on a quick route to the sump beyond, rescaled the avens to a higher point, and radio located a position to {{convert|50|ft|m}} below the moor. It was finally connected to the surface in June 2002.<ref>{{cite journal
+text_Aquamole_Pot = """Work restarted in 2000 when divers who were keen on a quick route to the sump beyond, rescaled the avens to a higher point, and radio located a position to {{convert|50|ft|m}} below the moor. It was finally connected to the surface in June 2002.<ref>{{cite journal
  | author = Rupe Skorupka
  | year = 2002
  | month = October
@@ -43,19 +17,92 @@ text = """Work restarted in 2000 when divers who were keen on a quick route to t
  | pages = page 20–22
  | url = http://www.wildplaces.co.uk/descent/descent168.html
  }}</ref>
- blah"""
+blah"""
 
-#import xml.etree.ElementTree as ET
-#tree = ET.fromstring(text)
-#for r in tree.findall('ref'):
-#    print(r.text)
+text_Aquamole_Pot_replaced_ref = """Work restarted in 2000 when divers who were keen on a quick route to the sump beyond, rescaled the avens to a higher point, and radio located a position to {{convert|50|ft|m}} below the moor. It was finally connected to the surface in June 2002. coeref0 
+blah"""
+
+text_Aquamole_Pot_2_refs = """Work restarted in 2000 when divers who were keen on a quick route to the sump beyond, rescaled the avens to a higher point, and radio located a position to {{convert|50|ft|m}} below the moor. It was finally connected to the surface in June 2002.<ref>{{cite journal
+ | author = Rupe Skorupka
+ | year = 2002
+ | month = October
+ | title = A Pot Into Aquamole
+ | journal = [[Descent (magazine)]]
+ | issn = 0046-0036
+ | issue = 168
+ | pages = page 20–22
+ | url = http://www.wildplaces.co.uk/descent/descent168.html
+ }}</ref><ref>{{cite journal
+ | author = Rupe Skorupka
+ | year = 2002
+ | month = October
+ | title = A Pot Into Aquamole
+ | journal = [[Descent (magazine)]]
+ | issn = 0046-0036
+ | issue = 168
+ | pages = page 20–22
+ | url = http://www.wildplaces.co.uk/descent/descent168.html
+ }}</ref>
+blah"""
+
+text_Aquamole_Pot_2_replaced_refs = """Work restarted in 2000 when divers who were keen on a quick route to the sump beyond, rescaled the avens to a higher point, and radio located a position to {{convert|50|ft|m}} below the moor. It was finally connected to the surface in June 2002. coeref0  coeref1 
+blah"""
+
+text_Aquamole_Pot_no_url = """Work restarted in 2000 when divers who were keen on a quick route to the sump beyond, rescaled the avens to a higher point, and radio located a position to {{convert|50|ft|m}} below the moor. It was finally connected to the surface in June 2002.<ref>{{cite journal
+ | author = Rupe Skorupka
+ | year = 2002
+ | month = October
+ | title = A Pot Into Aquamole
+ | journal = [[Descent (magazine)]]
+ | issn = 0046-0036
+ | issue = 168
+ | pages = page 20–22
+ }}</ref>
+blah"""
+
+text_Aquamole_Pot_no_url_without_ref = """Work restarted in 2000 when divers who were keen on a quick route to the sump beyond, rescaled the avens to a higher point, and radio located a position to {{convert|50|ft|m}} below the moor. It was finally connected to the surface in June 2002.
+blah"""
 
 
-from pprint import pprint
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(text)
+class TestAquamolePot(unittest.TestCase):
 
-refs = soup.find_all('ref')
-for r in refs:
-    r.replace_with('ref0')
-pprint(soup.text)
+   def setUp(self):
+       self.title = 'https://en.wikipedia.org/wiki/Aquamole_Pot'
+
+   def test_sentences(self):
+       wikitext = get_data.scrape_wikitext('Aquamole Pot')
+       expect = [
+           'Aquamole Pot is a cave in West Kingsdale, North Yorkshire, England.',
+           'It was originally explored from below by cave divers who had negotiated  of sump passage from Rowten Pot in 1974, to discover a high aven above the river passage.',
+           'History',
+           'The  aven was scaled in 1980 using poles, ladders and hand bolting kits, and a radio location transmitter placed at the highest point.',
+           'Having discovered it was  from, and  below Jingling Pot, the aven was renamed Aquamole Aven instead of Jingling Avens.',
+           'Work restarted in 2000 when divers who were keen on a quick route to the sump beyond, rescaled the avens to a higher point, and radio located a position to  below the moor.',
+           'It was finally connected to the surface in June 2002.'
+           ]
+       actual = get_data.split_sentences(wikitext)
+       self.assertEqual(expect, actual)
+
+
+@ddt
+class TestRefTag(unittest.TestCase):
+
+    @data(
+        (text_Aquamole_Pot_no_url, text_Aquamole_Pot_no_url_without_ref),
+        )
+    def test_remove_refs_with_no_url(self, value):
+        input_data, expect = value
+        self.assertEqual(expect, get_data.remove_refs_with_no_url(input_data))
+
+    @data(
+        (text_Aquamole_Pot, (text_Aquamole_Pot_replaced_ref, ['http://www.wildplaces.co.uk/descent/descent168.html'])),
+        (text_Aquamole_Pot_2_refs, (text_Aquamole_Pot_2_replaced_refs, ['http://www.wildplaces.co.uk/descent/descent168.html', 'http://www.wildplaces.co.uk/descent/descent168.html'])),
+        )
+    def test_remove_refs_with_url(self, value):
+        input_text, expect = value
+        expect_changed_text, expect_ref_urls = expect
+        actual_changed_text, actual_ref_urls = get_data.replace_refs_with_url(
+            input_text
+            ) 
+        self.assertEqual(expect_changed_text, actual_changed_text)
+        self.assertEqual(expect_ref_urls, actual_ref_urls)
